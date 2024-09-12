4fba531e200991346603fbd1b63729202988a2c1
brandonmsg <152921652+brandonmsg@users.noreply.github.com>
2024-09-11 18:53:18 -0700

    comments for current function

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import numpy as np
 #python ./graph.py --input graph_file.gml --create_random_graph 
 #--nodes n --constant c --plot --BFS a --output out_graph_file.gml
 
-def create_random_graph(nodes, constant):
-    probability = (constant * np.log(nodes)) / nodes
-    graph = nx.erdos_renyi_graph(nodes, probability)
+def create_random_graph(nodes, constant): # creates random graph using erdos renyl theorem
+    probability = (constant * np.log(nodes)) / nodes # probability for erdos renyl graph found from class notes
+    graph = nx.erdos_renyi_graph(nodes, probability) # netowrkx has function for erdos renyl graph
     return graph
 
-def read_graph(filename): 
-    file = nx.read_gml(filename)
+def read_graph(filename): # reads graph from file if there is one
+    file = nx.read_gml(filename) # networkx function for reading files
     return file
 
-def write_graph(graph, file): 
-    nx.write_gml(graph, file)   
+def write_graph(graph, file): # saves graph to file
+    nx.write_gml(graph, file) # networkx function for writing graph into file
     return 0
 
 def main():
